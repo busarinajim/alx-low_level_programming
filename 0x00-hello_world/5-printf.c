b52d562573b097a24618a1c09fc79c2803b7f0fa
busarinajim <busarinajim@gmail.com>
2023-10-09 02:59:48 -0700

    Printing in C using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * printing in C using printf function
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) /* main - Entry point */
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
