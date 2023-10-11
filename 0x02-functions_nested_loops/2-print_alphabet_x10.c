d68dc7de6343d82ac15f5d56a0a5503d7231139c
busarinajim <busarinajim@gmail.com>
2023-10-11 11:53:00 -0700

    Printing alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - To print alphabets using _putchar
+ * 
+ * print_alphabet_x10 - Prints alphabets 10x 
+ */
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+}
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i=0; i<10; i++)
+	{
+		print_alphabet(i);
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,16 @@
  * header file
  * contains declarations for my functions
  */
+#ifndef MAIN.H
+#define MAIN.H
 
 int _putchar(char c);
 
 /*print-alphabet prototype*/
 void print_alphabet(void);
 
+/*print_alphabet_X10 prototype*/
+void print_alphabet_x10(void);
+
+
+#endif
