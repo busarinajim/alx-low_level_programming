302733b9bcc7bc309478b1efd83117c022177dd2
busarinajim <busarinajim@gmail.com>
2023-10-11 04:50:27 -0700

    using _putchar function

---
+++ 0x02-functions_nested_loops/hello_puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	const char *str = "Hello, World!"
+		for (int i = 0; str[i] != '\0'; i++) {
+			        putchar(str[i]);
+				    }
+		    
+		    putchar('\n'); // Print a newline character at the end
+		        return 0;
+}
+
+
