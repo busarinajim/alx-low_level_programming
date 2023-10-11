e88ca76f0a7a8431b64fdbc5f6050cf0884183e8
busarinajim <busarinajim@gmail.com>
2023-10-11 09:40:40 -0700

    printing alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet function - To print alphabets using _putchar
+ * print_alphabet - To print alphabets using _putchar
  * No parameters
- *
- * main function - calls print_alphabet
- *
- * Return - 0 (success)
  */
 void print_alphabet(void)
 {
 	char alphabet = 'a';
 	while (alphabet <= 'z')
+	
 	{
 		_putchar(alphabet);
 		alphabet++;
