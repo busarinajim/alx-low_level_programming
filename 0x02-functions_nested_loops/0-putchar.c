f789bc9e6120bfc27170d5252f3089f77404f912
busarinajim <busarinajim@gmail.com>
2023-10-11 04:43:52 -0700

    printing _putchar using _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,14 @@
  */
 int main(void)
 {
-	const char *str="_putchar";
-	
-	for (int i=0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');	
 	return (0);
 }
