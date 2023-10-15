702eb0b3c93fc81cb8edd7b62c2424074f85f532
busarinajim <busarinajim@gmail.com>
2023-10-15 00:45:17 -0700

    printing to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,40 @@
 #include "main.h"
 
 /**
+ * print_int - prints integers using _putchar
+ * @num: integer to be printe
+ * Return: 0
+ */
+
+void print_int(int num)
+{
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (num == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	char buffer[10];
+	int index = 9;
+
+	while (num > 9)
+	{
+		buffer[index--] = num % 10 + '0';
+		num /= 10;
+	}
+	int i;
+
+	for (i = index + 1; i < 10; i++)
+	{
+		_putchar(buffer[i]);
+	}
+}
+
+/**
  * print_to_98 - prints n to 98
  * @n: integer to start printing from
  */
@@ -9,13 +43,16 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (int i = n; i <= 98; i++)
+		int i;
+		
+		for (i = n; i <= 98; i++)
 		{
-			_putchar("%d", i);
+			print_int(i);
 			if (i < 98)
 			{
-				_putchar(", ");
-			}
+				_putchar(",");
+				_putchar(" ");
+			}	
 			else
 			{
 				_putchar("\n");
@@ -24,12 +61,15 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (int i = n; i >= 98; i--)
+		int i;
+		
+		for (i = n; i >= 98; i--)
 		{
-			_putchar("%d", i);
+			print_int(i);
 			if (i > 98)
 			{
-				_putchar(", ");
+				_putchar(",");
+				_putchar(" ");
 			}
 			else
 			{
