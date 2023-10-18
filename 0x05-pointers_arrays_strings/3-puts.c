733f8b91f93730f124e05a73e49e6ffd70e82c7e
busarinajim <busarinajim@gmail.com>
2023-10-17 17:49:15 -0700

    printing a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to stdout
+ * @str: string to be printed
+ *
+ */
+
+void _puts(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		_putchar('str');
+		len++;
+	}
+	_putchar('\n');
+}
