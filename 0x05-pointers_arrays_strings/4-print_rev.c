d0c63d9b4ef12612d962f86a276f15e56edc7344
busarinajim <busarinajim@gmail.com>
2023-10-17 18:34:39 -0700

    printing string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,13 @@ void print_rev(char *s)
 {
 	int len;
 	int i;
-	
+
 	len = 0;
 	while (s[len] != 0)
 	{
 		len++;
 	}
-	for (i = len -1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
