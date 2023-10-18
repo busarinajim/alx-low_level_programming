978322564d0afbcb1bdaa5a894e8ec91c709f322
busarinajim <busarinajim@gmail.com>
2023-10-17 18:09:01 -0700

    printing string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse order
+ * @s: string to be printed
+ *
+ */
+
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = '\0'; len = *s; len--)
+	{
+		_putchar('len');
+	}
+	_putchar('\n')
+}
