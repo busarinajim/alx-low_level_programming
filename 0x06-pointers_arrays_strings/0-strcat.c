02af10ce8a44ca1995afb9a8d0990728944cacea
busarinajim <busarinajim@gmail.com>
2023-10-19 17:43:41 -0700

    concatenating 2 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *dest_ptr;
-
-	*dest_ptr = dest;
-	while (dest != '\0')
+	char *dest_ptr = dest;
+	
+	while (*dest != '\0')
 	{
 		dest++;
 	}
-	while (src != '\0')
+	while (*src != '\0')
 	{
 		*dest = *src;
 		dest++;
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,4 +2,4 @@
 
 char *_strcat(char *dest, char *src);
 
-
+int _putchar(char c);
