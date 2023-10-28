5507606bc00f60779c17301feb433f8c7945b931
busarinajim <busarinajim@gmail.com>
2023-10-27 19:44:14 -0700

    multiplying numbers passed as arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,15 +12,15 @@ int main(int argc, char **argv)
 {
 	int mul;
 
-	if (argc > 1)
+	if (argc == 3)
 	{
 		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
+		return (0);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	return (0);
 }
