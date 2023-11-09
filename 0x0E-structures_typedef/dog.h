d8626afb31c8fd12ae2cd51e2386b5857bc48bf3
busarinajim <busarinajim@gmail.com>
2023-11-09 08:08:13 -0800

    defining structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - defining stricture dog
+ * @name: parameter 1
+ * @age: parameter 2
+ * @owner: parameter 3
+ *
+ */
+
 struct dog
 {
 	char *name;
